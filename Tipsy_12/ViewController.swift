4daa2d29008eaaf3a3289d86cf0a1d9f4cfac1c6
Larisa Teregulova <larisa.andreeva.1995@mail.ru>
2023-04-25 09:24:28 +0300

    Initial Commit

---
+++ Tipsy_12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy_12
+//
+//  Created by Лариса Терегулова on 25.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
